a31d82d8fe1a48304b649f832ac7031bc620fd09
James Brown <jbrown@easypost.com>
2016-01-27 01:52:14 +0000

    add -B flag to generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -31,6 +31,7 @@ def main():
                         help='Size range of tasks to generate, in bytes (default %(default)s)')
     parser.add_argument('-d', '--delay-range', default='0', type=parse_range,
                         help='Size range of delays to generate, in seconds (default %(default)s)')
+    parser.add_argument('-B', '--binary', action='store_true', help='Make jobs random binary data')
     parser.add_argument('number_of_tasks_per_tube', type=int, help='Number of tasks to generate per tube')
     parser.add_argument('tubes', type=str, nargs='+', help='Tubes into which tasks should be inserted')
     args = parser.parse_args()
@@ -41,8 +42,11 @@ def main():
         for i in range(args.number_of_tasks_per_tube):
             data_size = random.randint(*args.task_size)
             data = non_cryptographically_random_bytes(data_size)
-            data = base64.b64encode(data).decode('ascii')
-            job = json.dumps({'sequence': i, 'data': data})
+            if args.binary:
+                job = data
+            else:
+                data = base64.b64encode(data).decode('ascii')
+                job = json.dumps({'sequence': i, 'data': data})
             priority = random.randrange(0, 1 << 31)
             delay = random.randint(*args.delay_range)
             state, job_id = client.put_job(job, pri=priority, delay=delay)
